83dd08a7bbbaf35fdb36ed61d8fa1ca9fcec5152
surajPanayanchery <suraj.panayanchery@gmail.com>
2019-09-14 16:08:26 +0530

    MOOC : part2 courseinfo 1 and 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    const course = 'Half Stack application development'
+    const exercises = [
+        { part: 'Fundamentals of React', exercise: 10 },
+        { part: 'Using props to pass data', exercise: 7 },
+        { part: 'State of a component', exercise: 14 }
+    ]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content exercises={exercises} />
+            <Footer count={exercises[0].exercise + exercises[1].exercise + exercises[2].exercise} />
+        </div>
+    )
+}
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
+const Footer = (props) => {
+    return (
+        <div>
+            <p>Number of exercises {props.count}</p>
+        </div>
+    )
+}
+
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.exercises[0].part} exercise={props.exercises[0].exercise} />
+            <Part part={props.exercises[1].part} exercise={props.exercises[1].exercise} />
+            <Part part={props.exercises[2].part} exercise={props.exercises[2].exercise} />
+        </div>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <div>
+            <p> {props.part} {props.exercise}</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
